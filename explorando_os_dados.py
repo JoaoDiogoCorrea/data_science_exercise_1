ddc03934767bf2a5ff9893bb7491f256d925f1c9
JoaoDiogoCorrea <joaodiogo@gmail.com>
2024-07-13 12:28:44 -0300

    Calculando a correlação e gerando o gráfico do calculo

--- explorando_os_dados.py
+++ explorando_os_dados.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 # Carregar os dados do arquivo CSV
@@ -25,12 +27,36 @@ correlacao = dados[['Vendas', 'Receita', 'Custos']].corr()
 print("Matriz de Correlação:")
 print(correlacao)
 
+# Configuração do tamanho do gráfico
+plt.figure(figsize=(8, 6))
+
+# Gerando o heatmap com seaborn
+sns.heatmap(correlacao, annot=True, cmap='coolwarm', fmt='.2f', annot_kws={"size": 12})
+
+# Adicionando título
+plt.title('Matriz de Correlação')
+
+#Salvando gráfico de correlação
+#plt.savefig('graficos/correlacao.png')
+
+# Exibindo o gráfico
+plt.show()
+
 
 # Plotar um histograma das vendas mensais
-import matplotlib.pyplot as plt
-plt.hist(dados['Vendas'], bins=20, edgecolor='black')
-plt.title('Distribuição das Vendas Mensais')
-plt.xlabel('Vendas')
-plt.ylabel('Frequência')
-plt.grid(True)
-plt.show()
+#plt.hist(dados['Vendas'], bins=20, edgecolor='black')
+#plt.title('Distribuição das Vendas Mensais')
+#plt.xlabel('Vendas')
+#plt.ylabel('Frequência')
+#plt.grid(True)
+#plt.show()
+
+# Gráfico de vendas por mês
+#y = dados['Vendas']
+#x = dados['datas']
+#plt.bar(x, y)
+#plt.xlabel('Valores de X')
+#plt.ylabel('Valores de Y')
+#plt.title('Gráfico de Linha')
+#plt.grid(True)
+#plt.show()
